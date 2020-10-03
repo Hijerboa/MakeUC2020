ff731f4596efa2b0cde351f5ebe7a187bfebcc61
Noxid00 <Noxid00@gmail.com>
2020-10-03 16:32:50 -0400

    Fixed some numbers

--- covid-tracker/report.py
+++ covid-tracker/report.py
@@ -1,4 +1,5 @@
 import functools
+import criteria_constants
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -16,18 +17,18 @@ def create():
         for i in range(num_criteria): 
             radio_str = 'criteria_' + str(i) + '_radio' # TODO Make this match the naming convention for the selector
             flag = int(request.form[radio_str])
-            if flag == 0: # Negative
+            if flag == -1: # Negative
                 flags[x] = 1
-            elif flag == 2: # Positive
+            elif flag == 1: # Positive
                 flags[x + 16] = 1
         
         flags_out = 0
         for x in enumerate(flags):
-            flags_out += x[1] << (32 - x[0])
+            flags_out += x[1] << (31 - x[0])
 
         report_text = request.form['textbox'] # TODO Make this match the naming convention for the selector
 
-        user = conn.execute(
+        report = conn.execute(
             'INSERT INTO reports (UID, LID, TIMESTAMP, FLAGS, TEXT) VALUES (?, ?, current_timestamp, ?, ?);', (g.UID, g.LID, flags_out, report_text,)
         ).fetchone()
 
@@ -37,4 +38,24 @@ def create():
 
     else:
        return render_template('report/create.html')
+
+@bp.route('/location', methods=('GET', 'POST'))
+def create():
+    num_criteria == 10
+
+    flags_list = conn.execute(
+            'SELECT (FLAGS) FROM REPORTS WHERE LID = ?;', (g.LID,)
+        ).fetchone()
+
+    flag_scores = [2.5 for x in range(num_criteria)]
+    for flag_set in flags_list:
+        for i in range(num_criteria):
+            if not (flag_set & (1 << (31-i))) == 0:
+                flag_scores[i] -= (1.0 / len(flags_list))
+            if not (flag_set & (1 << (15-i))) == 0:
+                flag_scores[i] += (1.0 / len(flags_list))
+
+    # TODO Maybe do things to weight the scores or something.
+
+    return render_template('report/location.html', scores=flag_scores)
     
