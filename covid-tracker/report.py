a3931f75a71f201441b715f8a2bc460bb278c09a
Christen Ford <christen0ford@gmail.com>
2020-10-03 14:40:14 -0400

    add in report file

---
+++ covid-tracker/report.py
@@ -0,0 +1,12 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from . import db
+
+bp = Blueprint('report', __name__, url_prefix='/report')
+
+@bp.route('/create', methods=('GET', 'POST'))
+def create():
+    pass
